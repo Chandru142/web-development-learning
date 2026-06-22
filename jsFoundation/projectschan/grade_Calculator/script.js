b5c412164d9b474374686768bcdbbe78c7a770b6
Chandru142 <chandrud217@gmail.com>
2026-06-22 15:36:10 +0530

    Day 2 JS project added

---
+++ jsFoundation/projectschan/grade_Calculator/script.js
@@ -0,0 +1,17 @@
+function calculateGrade() {
+    let marks = document.getElementById("marks").value;
+
+    let grade;
+
+    if (marks >= 90) {
+      grade = "A";
+    } else if (marks >= 75) {
+      grade = "B";
+    } else if (marks >= 50) {
+      grade = "C";
+    } else {
+      grade = "Fail";
+    }
+
+    document.getElementById("result").innerText = "Your Grade is: " + grade;
+}
